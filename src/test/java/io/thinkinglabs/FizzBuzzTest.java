fcd12968aacef6cac781aef95d2c6ff1929e9870
Thierry de Pauw <thierry@thinkinglabs.io>
2017-08-29 21:12:53 +0200

    test return 1 when input is 1

---
+++ src/test/java/io/thinkinglabs/FizzBuzzTest.java
@@ -0,0 +1,16 @@
+package io.thinkinglabs;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author @tdpauw
+ */
+public class FizzBuzzTest
+{
+    @Test
+    public void shouldReturn1WhenInputIs1() {
+        assertThat(1).isEqualTo(1);
+    }
+}
