edaab739451da179bf63a77018e8a889a8a4a51c
Thierry de Pauw <thierry@thinkinglabs.io>
2017-08-29 23:30:17 +0200

    test should return Fizz when input is 3

--- src/test/java/io/thinkinglabs/FizzBuzzTest.java
+++ src/test/java/io/thinkinglabs/FizzBuzzTest.java
@@ -1,5 +1,6 @@
 package io.thinkinglabs;
 
+import org.assertj.core.api.IntegerAssert;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -11,16 +12,24 @@ public class FizzBuzzTest
 {
     @Test
     public void shouldReturn1WhenInputIs1() {
-        assertThat(fizzBuzz(1)).isEqualTo(1);
+        assertThat(fizzBuzz(1)).isEqualTo("1");
     }
 
     @Test
     public void shouldReturn2WhenInputIs2() {
-        assertThat(fizzBuzz(2)).isEqualTo(2);
+        assertThat(fizzBuzz(2)).isEqualTo("2");
     }
 
-    private int fizzBuzz(int value)
+    @Test
+    public void shouldReturnFizzWhenInputIs3() {
+        assertThat(fizzBuzz(3)).isEqualTo("Fizz");
+    }
+
+    private String fizzBuzz(int value)
     {
-        return value;
+        if (value == 3) {
+            return "Fizz";
+        }
+        return Integer.toString(value);
     }
 }
